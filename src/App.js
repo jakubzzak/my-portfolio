1ae86b78801701ae10ee95f034d6d0987811a0a2
jakubzzak <zak.jakub67@gmail.com>
2020-07-11 02:22:16 +0200

    header

--- src/App.js
+++ src/App.js
@@ -1,30 +1,79 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { useState, Component } from 'react';
+import styles from './styles/App.module.css';
 import Main from './components/main';
 import { Link } from 'react-router-dom';
-import { Header, Content, Layout, Navigation } from "react-mdl";
-import { Icon } from "semantic-ui-react";
+import { Content, Layout } from "react-mdl";
+import {Menu, Icon, Segment, Container, Sticky} from "semantic-ui-react";
 
+
+const Header = () => {
+    const [current, setCurrent] = useState("home")
+
+    const handleItemClick = (e, { name }) => setCurrent(name);
+
+    return (
+        <Segment inverted >
+            <Menu inverted pointing secondary size={"small"} >
+                <Menu.Item
+                    icon='id badge'
+                    name='Me'
+                    active={current === 'Me'}
+                    onClick={handleItemClick}
+                    as={Link}
+                    to={'/'}
+                />
+                <Menu.Menu position='right'>
+                    <Menu.Item
+                        name='resume'
+                        active={current === 'resume'}
+                        onClick={handleItemClick}
+                        as={Link}
+                        to={'/resume'}
+                    />
+                    <Menu.Item
+                        name='aboutme'
+                        active={current === 'aboutme'}
+                        onClick={handleItemClick}
+                        as={Link}
+                        to={'/aboutme'}
+                    />
+                    <Menu.Item
+                        name='contact'
+                        active={current === 'contact'}
+                        onClick={handleItemClick}
+                        as={Link}
+                        to={'/contact'}
+                    />
+                    <Menu.Item
+                        name='projects'
+                        active={current === 'projects'}
+                        onClick={handleItemClick}
+                        as={Link}
+                        to={'/projects'}
+                    />
+                    <Menu.Item
+                        name='blog'
+                        active={current === 'blog'}
+                        onClick={handleItemClick}
+                        as={Link}
+                        to={'/blog'}
+                    />
+                </Menu.Menu>
+            </Menu>
+        </Segment>
+    )
+}
 
 class App extends Component {
 	render() {
 		return (
-			<div className="demo-big-content">
-				<Layout>
-					<Header className="header-color" title={<Link style={{textDecoration: 'none', color: 'white'}} to="/"><Icon name="id badge" />Jakub Žák</Link>} scroll>
-						<Navigation>
-							<Link to="/resume">Resume</Link>
-							<Link to="/aboutme">About Me</Link>
-							<Link to="/projects">Projects</Link>
-							<Link to="/contact">Contact</Link>
-							<Link to="/blog">Blog</Link>
-						</Navigation>
-					</Header>
-					<Content>
-						<div className="page-content" />
-						<Main/>
-					</Content>
-				</Layout>
+			<div style={{ margin: 0, padding: 0 }}>
+                <Sticky>
+                    <Header />
+                </Sticky>
+                <Container >
+                    <Main/>
+                </Container>
 			</div>
 		);
 	}

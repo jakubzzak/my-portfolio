3b05c10b6a25c9ddcd8eca7d3f2018d6a08872c2
jakubzzak <zak.jakub67@gmail.com>
2020-09-30 20:27:31 +0200

    projects design approved, missing desc

--- src/components/pages/projects.js
+++ src/components/pages/projects.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import {CardTitle, CardText, CardActions } from 'react-mdl';
-import {Card, Grid, GridRow as Row, GridColumn as Column, Header, Button } from "semantic-ui-react";
+import {CardTitle, CardText, CardActions} from 'react-mdl';
+import {Card, Grid, GridRow as Row, GridColumn as Column, Header, Button} from "semantic-ui-react";
 import dummy from '../../styles/Dummy.module.css'
+
+import acadsysImage from '../../assets/acadsys.png'
+import camasysImage from '../../assets/camasys.png'
+import rentalportImage from '../../assets/rentalport.png'
 
 
 const Projects = (props) => {
 
   return (
-    <Grid id={'Projects'} textAlign={"center"} style={{padding: '3em'}} className={dummy.template} >
+    <Grid id={'Projects'} textAlign={"center"} style={{padding: '3em'}}
+          className={dummy.template}>
       <Row>
-        <Column>
-          <Header style={{ marginBottom: '2em' }}>Projects</Header>
+        <Column width={16}>
+          <Header style={{marginBottom: '2em'}}>Projects</Header>
         </Column>
       </Row>
-      <Card.Group>
-        <Card style={{border: '2px black solid', borderRadius: '6px'}}>
+      <Card.Group centered>
+        <Card style={{
+          border: '1.5px black solid',
+          borderRadius: '3px',
+          width: '350px',
+          boxShadow: '0px 0px 15px 15px rgba(0, 0, 0, 0.1), 0px 0px 13px 10px rgba(0, 0, 0, 0.3), 0px 0px 10px 5px rgba(0, 0, 0, 0.5)',
+          margin: '2em'
+        }}>
           <CardTitle style={{
             color: '#fff',
-            height: '180px',
-            background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'
-          }}>Acadsys</CardTitle>
+            height: '200px',
+            background: `url(${acadsysImage}) center / cover`
+          }} />
           <CardText>
+            <div>Acadsys</div><br/>
             Kratucky popis
           </CardText>
           <CardActions>
@@ -30,14 +42,22 @@ const Projects = (props) => {
             </Button>
           </CardActions>
         </Card>
-        <Card style={{border: '2px black solid', borderRadius: '6px'}}>
+        <Card style={{
+          border: '1.5px black solid',
+          borderRadius: '3px',
+          width: '350px',
+          boxShadow: '0px 0px 15px 15px rgba(0, 0, 0, 0.1), 0px 0px 13px 10px rgba(0, 0, 0, 0.3), 0px 0px 10px 5px rgba(0, 0, 0, 0.5)',
+          margin: '2em'
+        }}>
           <CardTitle style={{
             color: '#fff',
-            height: '180px',
-            background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'
-          }}>Camasys</CardTitle>
+            height: '200px',
+            background: `url(${camasysImage}) center / cover`
+          }}/>
           <CardText>
-            Kratucky popis
+            <div>Camasys</div><br/>
+            <br/>
+            Kratucky popis..
           </CardText>
           <CardActions>
             <Button onClick={() => window.open('https://www.camasys.com/')}>
@@ -46,13 +66,20 @@ const Projects = (props) => {
             </Button>
           </CardActions>
         </Card>
-        <Card style={{border: '2px black solid', borderRadius: '6px'}}>
+        <Card style={{
+          border: '1.5px black solid',
+          borderRadius: '3px',
+          width: '350px',
+          boxShadow: '0px 0px 15px 15px rgba(0, 0, 0, 0.1), 0px 0px 13px 10px rgba(0, 0, 0, 0.3), 0px 0px 10px 5px rgba(0, 0, 0, 0.5)',
+          margin: '2em'
+        }}>
           <CardTitle style={{
             color: '#fff',
-            height: '180px',
-            background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'
-          }}>Rentalport</CardTitle>
+            height: '200px',
+            background: `url(${rentalportImage}) center / cover`
+          }} />
           <CardText>
+            <div>Rentalport</div><br/>
             Kratucky popis
           </CardText>
           <CardActions>
@@ -62,14 +89,21 @@ const Projects = (props) => {
             </Button>
           </CardActions>
         </Card>
-        <Card style={{border: '2px black solid', borderRadius: '6px'}}>
+        <Card style={{
+          border: '1.5px black solid',
+          borderRadius: '3px',
+          width: '350px',
+          boxShadow: '0px 0px 15px 15px rgba(0, 0, 0, 0.1), 0px 0px 13px 10px rgba(0, 0, 0, 0.3), 0px 0px 10px 5px rgba(0, 0, 0, 0.5)',
+          margin: '2em'
+        }}>
           <CardTitle style={{
             color: '#fff',
-            height: '180px',
-            background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'
-          }}>Test-eshop</CardTitle>
+            height: '200px',
+            background: `url(${camasysImage}) center / cover`
+          }} />
           <CardText>
-            Kratucky popis
+            <div>Cybik</div><br/>
+            Kratucky popis..
           </CardText>
           <CardActions>
             <Button onClick={() => window.open('https://test-eshop.netlify.app')}>
